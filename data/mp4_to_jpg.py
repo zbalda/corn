a33ef85ae49f5cec8c2663437ed03c77c3640ecb
Zachary Balda <zacharybalda23@gmail.com>
2019-10-25 19:14:36 -0400

    finish MP4 to jpg conversion script

--- data/mp4_to_jpg.py
+++ data/mp4_to_jpg.py
@@ -13,11 +13,8 @@ for r, d, f in os.walk("./"):
     for file in f:
         if '.MP4' in file:
             name = os.path.splitext(file)[0]
-            filepath = r[1:] + "/" + file
-
-            print(filepath)
 
             # generate image sequence and save as jpg images
-            #command = "ffmpeg -i " + filepath + " -vf fps=" + fps + " jpg/" + name + "_%02d.jpg"
-            #output = os.system(command)
-            #print(output)
+            command = "ffmpeg -i " + file + " -vf fps=" + fps + " " + name + "_%02d.jpg"
+            output = os.system(command)
+            print(output)
