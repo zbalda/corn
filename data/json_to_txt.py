f21d541f2012a990fe27c90c2a5a2960bc3a41a4
Zachary Balda <zacharybalda23@gmail.com>
2019-10-25 21:09:42 -0400

    json_to_txt.py works

--- data/json_to_txt.py
+++ data/json_to_txt.py
@@ -3,12 +3,10 @@ import subprocess
 import json
 import datetime
 
-print(os.getcwd())
-
 # create annotations file
 file_path = "../keras-frcnn/"
 file_name = "annotations_" + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + ".txt"
-print(file_path, file_name)
+print(file_path + file_name)
 annotations = open(file_path + file_name, "w+")
 
 # save annotations to file
@@ -23,7 +21,7 @@ for r, d, f in os.walk("./"):
                     annotations.write(image_path + ",")
                     for point in shape["points"]:
                         for val in point:
-                            annotations.write(str(val) + ",")
+                            annotations.write(str(round(val)) + ",")
                     annotations.write("corn\n")
 
 annotations.close()
